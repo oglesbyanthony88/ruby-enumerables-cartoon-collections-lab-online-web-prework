8e58493670b7841ce0407662fcb89b8f97d8b0e0
Anthony Oglesby <ogle1ant@gmail.com>
2019-08-09 18:31:15 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -13,7 +13,7 @@ def long_planeteer_calls(planeteer_calls)
 end
 
 def find_the_cheese(potentially_cheesy_items)
-  cheeses = %w[gouda cheddar camembert]
+  cheeses = ["gouda", "cheddar", "camembert"]
 
   potentially_cheesy_items.find do |maybe_cheese|
     cheeses.include?(maybe_cheese)
