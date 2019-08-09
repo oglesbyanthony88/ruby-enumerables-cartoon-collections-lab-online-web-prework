6eaf3056c1bbeecbfac6eaa0f13296d9e2b31ecc
Anthony Oglesby <ogle1ant@gmail.com>
2019-08-09 18:28:10 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -8,8 +8,8 @@ def summon_captain_planet(planeteer_calls)
   planeteer_calls.map { |calls| calls.capitalize + '!' }
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(planeteer_calls)
+  planeteer_calls.any? { |calls| calls.length > 4 }
 end
 
 def find_the_cheese# code an argument here
